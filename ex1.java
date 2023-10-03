4a03b58a8ec480e64932535792ef888850d99760
Felipe Silva Corrêa <lipesc41@gmail.com>
2023-10-03 14:07:25 -0300

    teste modified:   ex1.java

--- ex1.java
+++ ex1.java
@@ -1,34 +1,19 @@
-public class Pessoa {
-    private String nome;
-    public int idade;
+public class Livro(){
+    public String titulo;
+    public String autor;
+    public int ano;
     //Construtor
-    public Pessoa(String nome, int idade) {
+    public Livro(String titulo, String autor, int ano) {
         this.nome = nome;
         this.idade = idade;
     }
-    //metodos 
-    public void saudação() {
-        System.out.println("Olá, meu nome é " + nome + " e eu tenho " + idade + "anos.");
-    }
-    //metodo publico para acessar o atributo privado nome
-    public String getNome() {
-        return nome;
-    }
-    
-    //método publico para modifivcar o atributo privado nome
-    public void setNome(String novoNome) {
-        nome = novoNome;
-    }
-    
 }
 
-public class ExemploClasse {
-    public static void main(String[] args) {
-        //criando um objeto da classe Pessoa
-        Pessoa pessoa1 = new Pessoa("Alice", 25);
-        //acessando o atributo publico "idade"
-        System.out.println("Nome: " + pessoa1.idade);
-        
-        //usando o metodo publico pra acessar o atr4ibuto privado nome
-    }
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 }
