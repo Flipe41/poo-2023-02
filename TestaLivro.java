45738d4268aa321f0143e9e0bc820b170f150cd2
Felipe Silva Corrêa <lipesc41@gmail.com>
2023-10-03 14:47:56 -0300

    Update TestaLivro.java

--- TestaLivro.java
+++ TestaLivro.java
@@ -2,9 +2,9 @@
 public class TestaLivro {
     public static void main(String[] args) {
         // Criando instâncias dos livros
-        Livro livro1 = new Livro("The Da Vinci Code", "Dan Brown", 2003, "Doubleday");
-        Livro livro2 = new Livro("Título do Segundo Livro", "Autor do Segundo Livro", 2022, "Editora do Segundo Livro");
-        Livro livro3 = new Livro("Título do Terceiro Livro", "Autor do Terceiro Livro", 2021, "Editora do Terceiro Livro");
+        Livro livro1 = new Livro("A Vila dos Tecidos (A Vila dos Tecidos – Livro 1)", "Anne Jacobs", 2023, "Editora Arqueiro");
+        Livro livro2 = new Livro("Arrume a sua cama: Pequenas coisas que podem mudar a sua vida... E talvez o mundo", "William H. Mcraven", 2017, "Editora Academia");
+        Livro livro3 = new Livro("A coragem de não agradar: Como a filosofia pode ajudar você a se libertar da opinião dos outros, superar suas limitações e se tornar a pessoa que deseja", "Ichiro Kishimi", 2021, "Editora Sextante");
 
         // Exibindo o estado das instâncias
         System.out.println("Informações do Primeiro Livro:");
