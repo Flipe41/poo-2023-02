d87dfd342e44593d0f53598e97568ce3aeff3e90
Felipe Silva Corrêa <lipesc41@gmail.com>
2023-10-23 13:55:03 -0300

    update

---
+++ EX15/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Circulo circulo = new Circulo(5);
+        Retangulo retangulo = new Retangulo(4, 6);
+        Triangulo triangulo = new Triangulo(3, 8);
+
+        FormaGeometrica[] formas = {circulo, retangulo, triangulo};
+
+        double areaTotal = CalculadoraArea.calcularAreaTotal(formas);
+        System.out.println("A área total das formas é: " + areaTotal);
+    }
+}
